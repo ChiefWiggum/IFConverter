63bfa4729b750c65b7551b692cf92c27de234f0a
ChiefWiggum <The.Chief.Clancy.Wiggum@gmail.com>
2021-01-25 22:19:05 +0100

    added sln and proj files

---
+++ IFConverter.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IFConverter.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
